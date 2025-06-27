b28246c37bb4dc76195069888df025e2e47667de
thiagoeu <araujo.thiago1051@gmail.com>
2025-06-27 09:54:14 -0300

    feat: adicionado modulo de segurança e logs de requisições

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 import express from "express";
 import userRouter from "./routes.js";
+import morgan from "morgan";
+import helmet from "helmet";
 import { connectToDatabase } from "./utils/connectDB.js";
 import "dotenv/config";
 
 const app = express();
+
+// Logs de requisições HTTP
+app.use(morgan("dev"));
+
+// Segurança da aplicação
+app.use(helmet());
+
 app.use(express.json());
 app.use("/users", userRouter);
 
+const PORT = process.env.APP_PORT || 3000;
+
 connectToDatabase().then(() =>
-  app.listen(process.env.APP_PORT, () =>
-    console.log("Server running on port 3000")
-  )
+  app.listen(PORT, () => console.log("Server running on port 3000"))
 );
