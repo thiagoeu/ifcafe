20fcc80833b7f7fa824332c8f6d15dfe67293188
thiagoeu <araujo.thiago1051@gmail.com>
2025-06-27 12:24:23 -0300

    feat: criar usuario

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,55 @@
-import User from "../models/User.js";
-import crypto from "node:crypto";
+import User from "../models/User.model.js";
+import bcrypt from "bcrypt";
 
 export const userRegister = async (req, res) => {
   try {
-    const crateUser = {
-      id: crypto.randomUUID(),
-      ...req.body,
+    const { name, email, password } = req.body;
+
+    // Verifica se todos os campos obrigatórios estão presentes
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        error: true,
+        success: false,
+        message: "name, email, age and password are required",
+      });
+    }
+
+    // Verifica se o usuário já existe
+    const userExists = await User.findOne({ where: { email } });
+
+    if (userExists) {
+      return res.status(409).json({
+        error: true,
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const payload = {
+      name,
+      email,
+      password: hashedPassword,
     };
 
-    const newUser = await User.create(crateUser);
-    res.status(201).send(newUser);
+    // Cria novo usuário
+    const newUser = await User.create(payload);
+
+    return res.status(201).json({
+      success: true,
+      error: false,
+      message: "User created successfully",
+      user: newUser,
+    });
   } catch (error) {
-    res.status(500).send(error.message);
+    return res.status(500).json({
+      error: true,
+      success: false,
+      message: "Internal server error",
+      detail: error.message,
+    });
   }
 };
 
